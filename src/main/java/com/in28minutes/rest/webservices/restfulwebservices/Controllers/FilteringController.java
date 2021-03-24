44ce365fc09251b0e1c8b4364c1680249a035957
Marcos.Arce <marcos.arce@kinandcarta.com>
2021-03-24 12:12:35 -0300

    Second commit

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/Controllers/FilteringController.java
@@ -0,0 +1,35 @@
+package com.in28minutes.rest.webservices.restfulwebservices.Controllers;
+
+import com.fasterxml.jackson.databind.ser.BeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import com.in28minutes.rest.webservices.restfulwebservices.Beans.SomeBean;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class FilteringController {
+
+    @GetMapping("/filtering")
+    public MappingJacksonValue retrieveSomeBean() {
+        SomeBean someBean = new SomeBean("value1", "value2", "value3");
+
+        BeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1", "field2");
+        FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(someBean);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+
+    @GetMapping("/filtering-list")
+    public List<SomeBean> retrieveListOfSomeBean() {
+        return Arrays.asList(new SomeBean("value1", "value2", "value3"), new SomeBean("value4", "value5", "value6"));
+    }
+}
